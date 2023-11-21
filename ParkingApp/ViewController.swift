12321e2257476ebfbeaafbff6b2e6bc488d557d8
funMango <bluemango608@gmail.com>
2023-11-21 20:56:39 +0900

    feat: 현제위치 오버레이 구현

--- ParkingApp/ViewController.swift
+++ ParkingApp/ViewController.swift
@@ -8,14 +8,18 @@
 import UIKit
 import NMapsMap
 
+// https://jkim68888.tistory.com/5
+
 class ViewController: UIViewController, CLLocationManagerDelegate {
     var locationManager = CLLocationManager()
+    var locationOverlay: NMFLocationOverlay?
     
     private lazy var naverMapView: NMFMapView = {
         let mapView = NMFMapView()
         mapView.allowsZooming = true
         mapView.logoInteractionEnabled = false
         mapView.allowsScrolling = true
+        self.locationOverlay = mapView.locationOverlay
         return mapView
     }()
 
@@ -38,17 +42,29 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.startUpdatingLocation()
             print(locationManager.location?.coordinate as Any)
             
+            
             // 카메라 설정
-            let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0,
-                                                                   lng: locationManager.location?.coordinate.longitude ?? 0))
-            cameraUpdate.animation = .easeIn
+            let latitude = locationManager.location?.coordinate.latitude ?? 0
+            let longitude = locationManager.location?.coordinate.longitude ?? 0
+            let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: latitude, lng: longitude), zoomTo: 14)
             naverMapView.moveCamera(cameraUpdate)
+            cameraUpdate.animation = .easeIn
+            
+            
+            // 오버레이를 내 위치의 위도,경도로 설정
+            guard let locationOverlay = self.locationOverlay else { return }
+            locationOverlay.hidden = false
+            locationOverlay.location = NMGLatLng(lat: latitude, lng: longitude)
+            locationOverlay.icon = NMFOverlayImage(name: "marker3")
+                
             
             // 마커 설정
-            let marker = NMFMarker()
-            marker.position = NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0,
-                                        lng: locationManager.location?.coordinate.longitude ?? 0)
-            marker.mapView = naverMapView
+//            let marker = NMFMarker()
+//            marker.position = NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0,
+//                                        lng: locationManager.location?.coordinate.longitude ?? 0)
+//            marker.mapView = naverMapView
+            
+            
         } else {
             print("위치 서비스 Off 상태")
         }
