8098f37fbb9b10d327354e5b8f2c01b44f61159e
funMango <bluemango608@gmail.com>
2023-11-22 11:38:26 +0900

    feat: 현제 화면 중심 좌표 구현 사용자로인해 화면이 움직일때 현제 화면의 중심좌표 구현

--- ParkingApp/ViewController.swift
+++ ParkingApp/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         setUI()
         configCurLocation()
         setLocationData()
+        naverMapView.addCameraDelegate(delegate: self)
     }
     
     func configCurLocation() {
@@ -49,8 +50,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: latitude, lng: longitude), zoomTo: 14)
             naverMapView.moveCamera(cameraUpdate)
             cameraUpdate.animation = .easeIn
-            
-            
+                                    
             // 오버레이를 내 위치의 위도,경도로 설정
             guard let locationOverlay = self.locationOverlay else { return }
             locationOverlay.hidden = false
@@ -81,6 +81,27 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             naverMapView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
             naverMapView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor),
         ])
+    }
+}
+
+
+extension ViewController: NMFMapViewTouchDelegate, NMFMapViewCameraDelegate {
+   // 카메라 컨트롤
+   func mapView(_ mapView: NMFMapView, cameraDidChangeByReason reason: Int, animated: Bool) {
+       switch reason {
+       case -1:
+           let lat = mapView.cameraPosition.target.lat
+           let long = mapView.cameraPosition.target.lng
+           print("lat: \(lat)")
+           print("long: \(long)")
+       default:
+           return
+       }
+        // 두번째 파라미터 reason
+        // 0 : 개발자가 API를 호출해 카메라가 움직였음을 나타내는 값.
+        // -1 : 사용자의 제스처로 인해 카메라가 움직였음을 나타내는 값.
+        // -2 : 사용자의 버튼 선택으로 인해 카메라가 움직였음을 나타내는 값.
+        // -3 : 위치 정보 갱신으로 카메라가 움직였음을 나타내는 값.
     }
 }
 
